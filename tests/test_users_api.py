cb568076118124b43e50eec8bfa6d098558cb33c
missy1112 <misslanzunza@outlook.com>
2026-06-11 14:13:05 +0200

    Add API POST test

--- tests/test_users_api.py
+++ tests/test_users_api.py
@@ -26,3 +26,24 @@ def test_get_list_of_posts():
     assert "userId" in data[0]
     assert "title" in data[0]
     assert "body" in data[0]
+
+def test_create_new_post():
+    new_post = {
+        "title": "My first API test",
+        "body": "This post was created using an automated API test.",
+        "userId": 1
+    }
+
+    response = requests.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        json=new_post
+    )
+
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data["title"] == "My first API test"
+    assert data["body"] == "This post was created using an automated API test."
+    assert data["userId"] == 1
+    assert "id" in data
