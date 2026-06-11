f3a3879e5702bfbd4629d75ecd186f837b5c670a
missy1112 <misslanzunza@outlook.com>
2026-06-11 14:10:14 +0200

    Add API test for list of posts

--- tests/test_users_api.py
+++ tests/test_users_api.py
@@ -12,3 +12,17 @@ def test_get_single_post():
     assert data["userId"] == 1
     assert data["title"] != ""
     assert data["body"] != ""
+
+def test_get_list_of_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert isinstance(data, list)
+    assert len(data) > 0
+    assert "id" in data[0]
+    assert "userId" in data[0]
+    assert "title" in data[0]
+    assert "body" in data[0]
